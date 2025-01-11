ef64fdb2bd803d22bd58605bbb7f964cd63fa00d
Muskantam26 <mskntmrkr@gmail.com>
2025-01-11 17:58:31 +0530

    update

--- string.js
+++ string.js
@@ -1,14 +1,51 @@
- function muskan(){
- alert("please sign in")
-}
+//  function muskan(){
+//  alert("please sign in")
+// }
 // muskan();
 
-function ishika(){
-alert("ishika")
-}
-//ishika();
+// function ishika(){
+// alert("ishika")
+// }
+// //ishika();
 
-function sign(){
-alert("do you want to sign up")
-}
+// function sign(){
+// alert("do you want to sign up")
+// }
 //sign();
+
+// ===========================================day 2=========================================================
+
+function string(){
+    //alert("hey") :show on browser window
+    // console.log("hello") : show on console
+     document.write("<h1>hey</h1>")//show on html window
+
+}
+string();
+
+// ===================method of string=============================
+
+function day2(){
+let text="javascriptiseasy"
+// console.log(typeof(text));
+// document.write(text.length): string to check length of string.
+// document.write(text.slice(4,13))//slice method
+let text1="sid"
+let text2="JAVASCRIPT"
+// document.write(text1.concat(text2))//concat method
+document.write(text.toUpperCase())//uppercase method
+document.write(text2.toLowerCase())//lowercase method
+}
+//day2();
+
+
+function day3(){
+    let text1="hello-developer"
+     console.log(text1.charAt(1));
+     console.log(text1.charCodeAt(0));
+     console.log(text1.split("-"));//string convert to array
+     console.log(text1.indexOf("o"));
+    
+    }
+    day3();
+
