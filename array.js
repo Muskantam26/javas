f325ec11d9fd0a1cdb2a9996a70477839317f461
Muskantam26 <mskntmrkr@gmail.com>
2025-01-13 19:25:58 +0530

    new data arrived

---
+++ array.js
@@ -0,0 +1,18 @@
+function bid(){
+    let arr=["Apple","Mango","Banana","Grapes"]
+    console.log(arr.length);
+    // arr.pop() //  remove last element
+    // arr.pop("Grapes")
+    // console.log(arr);
+   
+    // arr.push("papaya", "pineapple")
+    // console.log(arr);
+
+    // arr.shift()
+    // console.log(arr);
+
+     arr.unshift("fruits")
+     console.log(arr);
+
+
+}
--- string.js
+++ string.js
@@ -26,15 +26,19 @@ string();
 // ===================method of string=============================
 
 function day2(){
-let text="javascriptiseasy"
+// let text="javascriptiseasy"
 // console.log(typeof(text));
 // document.write(text.length): string to check length of string.
 // document.write(text.slice(4,13))//slice method
-let text1="sid"
-let text2="JAVASCRIPT"
+// let text1="sid"
+// let text2="JAVASCRIPT"
 // document.write(text1.concat(text2))//concat method
-document.write(text.toUpperCase())//uppercase method
-document.write(text2.toLowerCase())//lowercase method
+// document.write(text.toUpperCase())//uppercase method
+// document.write(text2.toLowerCase())//lowercase method
+
+let text3="    JAVASCRIPT          "
+console.log(text3.trim());
+
 }
 //day2();
 
@@ -53,13 +57,43 @@ function day2(){
 
     function day3(){
           let text="I am learning javascript and Javascript is fum"
-          document.write(text.replace("javascript","JS"))
-          document.write(text.replace(/javascript/g,"JS"))//this will change to  same name as javascript
-         document.write(text.replace(/javascript/gi,"JS"))  
+        //   document.write(text.replace("javascript","JS"))
+        //   document.write(text.replace(/javascript/g,"JS"))//this will change to  same name as javascript
+        //  document.write(text.replace(/javascript/gi,"JS"))  
 
 
           //global=>g    i=>case insenstive 
 
         //   let arr=[2,4,5,6,"muskan"]//array
+
     }
    // day3();
+
+// function hm(){
+//   alert("This is \"JS\" and \"js\" is good" )
+// }
+// hm();
+
+// ============================================================Day4====================================================================
+
+//                                       Template letrals:
+// ========================================================================================================
+function day4(){
+
+// let text=`This is "JS" and "js" is good`
+// let text=`This is "JS" 
+// and "js" is good`
+let text=`This is "JS" <h1>and "js" is</h1> good`
+document.write(text)
+let name="muskan"
+let age=20;
+// let ans="hello" +name+ "and your age is "+age;
+let ans=`hello ${name} your age is ${age}`
+
+document.write(ans)
+// alert(text)
+// support single qoutes ('')and double qoutes ("").
+// support multiline string.
+// support html tags.
+}
+// day4();
