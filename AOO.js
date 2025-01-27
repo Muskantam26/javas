40cb9f7efb86e5cd1087d38513c677aab678d840
Muskantam26 <mskntmrkr@gmail.com>
2025-01-27 17:29:15 +0530

    update

--- AOO.js
+++ AOO.js
@@ -2,3 +2,35 @@
 // [{},{},{},{}]
 
 
+// map method :
+
+let a=[3,7,1,9,5,6]
+
+// let ans=a.map((e)=>{
+
+//     return e*2
+   
+// })
+// console.log(ans);
+
+// filter method :
+
+// let ans=a.filter((e)=>{
+
+//     // return e>5
+
+//     return (e%2==0)
+// })
+
+// console.log(ans);
+// for each method:return to string:
+
+let ans =a.forEach((e)=>{
+
+    console.log(e*2);
+
+})
+
+
+
+
