8f9aca6308af9ba1865250da054df149578124f2
Muskantam26 <mskntmrkr@gmail.com>
2025-02-12 17:13:10 +0530

    update

---
+++ date.js
@@ -0,0 +1,15 @@
+console.log(new Date());   //print current date    new keyword for object  format 
+
+let time = new Date()
+console.log(time.toLocaleString())   //readable
+console.log(time.getFullYear())      //year 2025
+
+console.log(time.getMonth())          //0 to 11   jan to dec
+
+console.log(time.getDay())            //0 to 6  sunday to sat
+
+console.log(time.getHours())
+console.log(time.getMinutes())
+
+console.log(time.getSeconds())
+console.log(time.getMilliseconds())
