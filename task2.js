5ba9181dc5ec93f2d86691618b832ec10e806e3f
Muskantam26 <mskntmrkr@gmail.com>
2025-02-13 16:48:03 +0530

    update

--- task2.js
+++ task2.js
@@ -1,25 +1,27 @@
-// let hm=()=>{
+let hm=()=>{
 
-//         let time = new Date()
+        let time = new Date()
 
-//     let age=document.querySelector("#agg")
-//     let a=document.querySelector("#ag").value
+    let age=document.querySelector("#agg")
+    let a=document.querySelector("#ag").value
    
-//     age.innerHTML=time.getFullYear()-a
+    age.innerHTML=time.getFullYear()-a
 
-// }
-
-let inter=setInterval((h) => {
-    
-    let hmm=document.querySelector("#inp").value
-   let hy=document.querySelector("#hh")
-
-hy.innerHTML=hmm;
---hmm;
-
-
-
-
-
-
-}, 2000);
+}
+let stop;
+count = 0;
+let h=()=>{
+    let out = document.querySelector("#time2");
+    let inp = document.querySelector("#time").value;
+     if(inp>=1){
+        count = inp;
+        out.innerHTML = count;
+       stop = setInterval(()=>{
+            count--;
+            out.innerHTML=count;
+            if(count==1){
+              clearInterval(stop)
+            }
+        },1000)
+     }
+};
