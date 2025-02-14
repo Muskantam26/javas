94cfba83c53c4a4decaf47fbdf5c0ac14ddf18da
Muskantam26 <mskntmrkr@gmail.com>
2025-02-14 14:46:17 +0530

    update

--- task2.js
+++ task2.js
@@ -1,30 +1,30 @@
-let hm=()=>{
+// let hm=()=>{
 
-        let time = new Date()
+//         let time = new Date()
 
-    let age=document.querySelector("#agg")
-    let a=document.querySelector("#ag").value
+//     let age=document.querySelector("#agg")
+//     let a=document.querySelector("#ag").value
    
-    age.innerHTML=time.getFullYear()-a
+//     age.innerHTML=time.getFullYear()-a
 
-}
-let stop;
-count = 0;
-let h=()=>{
-    let out = document.querySelector("#time2");
-    let inp = document.querySelector("#time").value;
-     if(inp>=1){
-        count = inp;
-        out.innerHTML = count;
-       stop = setInterval(()=>{
-            count--;
-            out.innerHTML=count;
-            if(count==1){
-              clearInterval(stop)
-            }
-        },1000)
-     }
-};
+// }
+// let stop;
+// count = 0;
+// let h=()=>{
+//     let out = document.querySelector("#time2");
+//     let inp = document.querySelector("#time").value;
+//      if(inp>=1){
+//         count = inp;
+//         out.innerHTML = count;
+//        stop = setInterval(()=>{
+//             count--;
+//             out.innerHTML=count;
+//             if(count==1){
+//               clearInterval(stop)
+//             }
+//         },1000)
+//      }
+// };
 
 // let tim=()=>{
 //   let time =new Date;
@@ -35,15 +35,36 @@ let h=()=>{
     
 // }
 
-let alarm=setInterval((clock)=>{
+// alarm clock
+
+
+let c=null;
+let a;
+let clock=()=>{
+   c=document.querySelector("#alarmtime").value
+   a=document.querySelector("#status")
+
+   
+if(!(c.includes(':'))){
+  a.innerHTML = "Please enter a valid time!";
+  return;
+}
+  a.innerHTML="alaram set for"+c;
+};
+let stop=setInterval(()=>{
   let time=new Date;
-  let cc=document.querySelector("#digital")
-    cc.innerHTML=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
-
-
   
-},2000)
 
+  let cu=`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+  console.log(cu)
 
+  if(c==cu){
+    alert("time to wake up");
+  }
+  if(c==cu){
+    clearInterval(stop)
+  }
+ 
+},1000);
 
 
